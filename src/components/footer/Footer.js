234edb044256f38be09084d24673e2888b6b863b
vijaychauhan54 <105157733+vijaychauhan54@users.noreply.github.com>
2022-11-04 13:54:20 +0530

    Update Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,8 +6,8 @@ function Footer() {
     <footer className="footerContainer">
       <div>
         <h3>
-          ©crybaby26_2022 Vishal Singh Chauhan. All rights reserved under
-          section 20001V-2202S-2802B.
+          Vijay . All rights reserved under
+          section 20001-2202S-2802B.
         </h3>
       </div>
     </footer>
